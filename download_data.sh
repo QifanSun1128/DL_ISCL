bb854b2f2ff8116f0859c56fadf86c52ccf4ab00
Jiarui Chen <jchen324@jh.edu>
2023-12-03 12:50:48 -0500

    Make the code compatible with colab

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-mkdir data data/multi
-cd data/multi
+mkdir $PWD/data $PWD/data/multi
+cd $PWD/data/multi
 wget http://csr.bu.edu/ftp/visda/2019/multi-source/real.zip -O real.zip
 wget http://csr.bu.edu/ftp/visda/2019/multi-source/sketch.zip -O sketch.zip
 unzip real.zip
