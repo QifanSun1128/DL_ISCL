553e1949675d5e14b49b0a4f46466d1f13dae628
hantaohu <hhu32@jh.edu>
2023-12-12 13:08:58 -0500

    diff model

--- utils/loss.py
+++ utils/loss.py
@@ -41,7 +41,7 @@ def adentropy(F1, feat, lamda, eta=1.0):
     loss_adent = lamda * torch.mean(torch.sum(out_t1 * (torch.log(out_t1 + 1e-5)), 1))
     return loss_adent
 
-
+'''
 class ConLoss(nn.Module):
     """Contrastive Learning Loss"""
 
@@ -94,3 +94,70 @@ class ConLoss(nn.Module):
                     reg_loss += torch.norm(z_i)
 
         return (total_loss+reg_loss) / len(group_source)
+'''
+
+class ConLoss(nn.Module):
+    """Contrastive Learning Loss"""
+
+    def __init__(self, temperature=0.07, margin=0.5, lambda_reg = 0.1):
+        super(ConLoss, self).__init__()
+        self.temperature = temperature
+        self.margin = margin
+        self.lambda_reg = lambda_reg
+
+    def forward(self, group_source, group_target):
+        """
+        Compute loss for model.
+        Args:
+            group_source: source dictionary
+            group_target: target dictionary.
+        Returns:
+            A loss scalar.
+        """
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        total_loss = torch.tensor(0.0, device=device)
+        reg_loss = torch.tensor(0.0, device=device) 
+        
+        z_a_target = []
+        z_a_source = []
+
+        if not group_source or not group_target:
+            raise ValueError("Source and target groups should not be empty")
+
+        for key in group_source.keys():
+            z_a_source.append(torch.stack(group_source[key]))
+        for key in group_target.keys():
+            z_a_target.append(torch.stack(group_target[key]))
+
+        z_a_target = torch.cat(z_a_target, dim=0).to(device)
+        z_a_source = torch.cat(z_a_source, dim=0).to(device)
+        z_a = torch.cat([z_a_target, z_a_source], dim=0).to(device)
+
+        for k in group_source.keys():
+            Z_j = torch.stack(group_source[k]).to(device)
+            if k in group_target.keys():
+                for z_i in group_target[k]:
+                    z_i = z_i.to(device)
+                    den = torch.exp(torch.matmul(z_a_source, z_i.T) / self.temperature).sum()
+                    den -= torch.exp(torch.dot(z_i, z_i) / self.temperature)
+                    num = torch.exp(torch.matmul(Z_j, z_i.T) / self.temperature)
+
+                    log_prob = F.relu(num - den + self.margin)
+                    total_loss -= torch.mean(log_prob)
+                    reg_loss += torch.norm(z_i)
+
+        for k in group_target.keys():
+            Z_j = torch.stack(group_target[k]).to(device)
+            if k in group_source.keys():
+                for z_i in group_source[k]:
+                    z_i = z_i.to(device)
+                    den = torch.exp(torch.matmul(z_a_target, z_i.T) / self.temperature).sum()
+                    den -= torch.exp(torch.dot(z_i, z_i) / self.temperature)
+                    num = torch.exp(torch.matmul(Z_j, z_i.T) / self.temperature)
+
+                    log_prob = F.relu(num - den + self.margin)
+                    total_loss -= torch.mean(log_prob)
+                    reg_loss += torch.norm(z_i)
+
+        return (total_loss) / (2*len(group_source))
