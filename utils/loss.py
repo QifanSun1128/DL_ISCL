999083096986b325a8e197ace37bf0cd1ed0dec7
hantaohu <hhu32@jh.edu>
2023-12-12 14:52:12 -0500

    changes

--- utils/loss.py
+++ utils/loss.py
@@ -157,4 +157,4 @@ class ConLoss(nn.Module):
                     log_prob = torch.log(num) - torch.log(den)
                     total_loss -= torch.mean(log_prob)
 
-        return 0.5*(total_loss) / z_a.shape[0]
+        return (total_loss) / z_a.shape[0]
